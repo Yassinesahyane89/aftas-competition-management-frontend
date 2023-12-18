1b93e31f4db8767a95ac75ee92c7ee27c63b0e16
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 23:22:17 +0100

    init FishListComponent

--- src/app/main/fish/pages/fish-list/fish-list.component.ts
+++ src/app/main/fish/pages/fish-list/fish-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { ColumnMode, DatatableComponent } from '@swimlane/ngx-datatable';
+import {Router} from "@angular/router";
+
+//services
+import {FishService} from "../../service/fish.service";
 
 @Component({
-  selector: 'app-fish-list',
-  templateUrl: './fish-list.component.html',
-  styleUrls: ['./fish-list.component.scss']
+    selector: 'app-fish-list',
+    templateUrl: './fish-list.component.html',
+    styleUrls: ['./fish-list.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class FishListComponent implements OnInit {
+    public selectedOption = 10;
+    public searchValue = '';
+    public ColumnMode = ColumnMode;
+    public fishList : any = [];
 
-  constructor() { }
+    // Decorator
+    @ViewChild(DatatableComponent) table: DatatableComponent;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private router: Router,
+        private fishService: FishService,
+    ) { }
+
+    ngOnInit(): void {
+    }
 
 }
