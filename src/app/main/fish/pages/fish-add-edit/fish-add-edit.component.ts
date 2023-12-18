0fbe744936477cd2585dcd38a773936f4f287492
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 23:58:56 +0100

    init FishAddEditComponent

--- src/app/main/fish/pages/fish-add-edit/fish-add-edit.component.ts
+++ src/app/main/fish/pages/fish-add-edit/fish-add-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
+// services
+import {FishService} from "../../service/fish.service";
+import {LevelService} from "../../../level/service/level.service";
 
 @Component({
   selector: 'app-fish-add-edit',
   templateUrl: './fish-add-edit.component.html',
-  styleUrls: ['./fish-add-edit.component.scss']
+  styleUrls: ['./fish-add-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class FishAddEditComponent implements OnInit {
+    public contentHeader: object;
+    public name = "";
+    public weight : number;
+    public levelId : number;
+    public levelOptions: { id: number, description: string }[] = [];
+    public selectedLevelId: number= null;
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private fishService: FishService,
+        private levelService: LevelService,
+    ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
