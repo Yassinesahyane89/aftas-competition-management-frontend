32d0b07742404288520a2faf9ddc0bb3a5e62bee
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 23:07:59 +0100

    init my FishService

--- src/app/main/fish/service/fish.service.ts
+++ src/app/main/fish/service/fish.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FishService {
+  public FishList: any ;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
