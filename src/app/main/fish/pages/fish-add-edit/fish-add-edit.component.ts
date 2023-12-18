9a38d0505455172891561e68ec32794333e6ced5
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 22:47:30 +0100

    add component for fish for add and edit

---
+++ src/app/main/fish/pages/fish-add-edit/fish-add-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fish-add-edit',
+  templateUrl: './fish-add-edit.component.html',
+  styleUrls: ['./fish-add-edit.component.scss']
+})
+export class FishAddEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
