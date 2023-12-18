61111e7bd9cd97dd2f5fba2325760279c503deb0
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 14:05:09 +0100

    add methode getAllLevel to get alla level from backend

--- src/app/main/level/service/level.service.ts
+++ src/app/main/level/service/level.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LevelService {
+  public LevelList: any ;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllLevel() {
+    return this.http.get(environment.levelsUrl);
+  }
 }
