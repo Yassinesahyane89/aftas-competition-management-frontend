aeadaf5a30773ce7b361390f01c1fd03247a4168
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 10:47:08 +0100

    add routing for page level in LevelModule

--- src/app/main/level/level.module.ts
+++ src/app/main/level/level.module.ts
@@ -1,11 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from "@angular/router";
 
 
 // level
 import { LevelListComponent } from "./pages/level-list/level-list.component";
 import {LevelAddEditComponent} from "./pages/level-add-edit/level-add-edit.component";
 
+// routing
+const routes: Routes = [
+    {
+        path: "list",
+        component: LevelListComponent,
+        data: { animation: "LevelListComponent" },
+    },
+    {
+        path: "add",
+        component: LevelAddEditComponent,
+        data: { animation: "LevelAddEditComponent" },
+    },
+    {
+        path: "edit/:id",
+        component: LevelAddEditComponent,
+        data: { animation: "LevelAddEditComponent" },
+    },
+    {
+        path: "**",
+        redirectTo: "list",
+        pathMatch: "full",
+    },
+  ];
 @NgModule({
   declarations: [LevelListComponent, LevelAddEditComponent],
   imports: [
