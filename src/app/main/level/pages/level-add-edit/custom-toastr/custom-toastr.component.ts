1c89a0012bd39d30143fb55206d085df59b2499c
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 17:02:20 +0100

    solve problem of view of custom-toastr.component.ts

--- src/app/main/level/pages/level-add-edit/custom-toastr/custom-toastr.component.ts
+++ src/app/main/level/pages/level-add-edit/custom-toastr/custom-toastr.component.ts
@@ -4,13 +4,12 @@ import { Toast, ToastrService, ToastPackage } from 'ngx-toastr';
 
 import { toastrSlideY } from './custom-toastr.animation';
 @Component({
-  selector: 'app-custom-toastr-component',
+  selector: '[app-custom-toastr-component]',
   templateUrl: './custom-toastr.component.html',
-  styleUrls: ['./custom-toastr.component.scss'],
   animations: [toastrSlideY],
   preserveWhitespaces: false
 })
-export class CustomToastrComponent extends Toast{
+export class CustomToastrComponent extends Toast {
   constructor(protected toastrService: ToastrService, public toastPackage: ToastPackage) {
     super(toastrService, toastPackage);
   }
