794f214dea06aead6742c72721fad4629d5a9f8b
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 00:15:45 +0100

    init component item of competition

---
+++ src/app/main/competition/pages/competition-item/competition-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-item',
+  templateUrl: './competition-item.component.html',
+  styleUrls: ['./competition-item.component.scss']
+})
+export class CompetitionItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
