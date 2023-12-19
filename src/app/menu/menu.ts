6fd22a9d8e630862e1df8f3c20707dad7369d110
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 00:23:32 +0100

    add competition in the menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,46 +1,46 @@
 import { CoreMenu } from '@core/types'
 
 export const menu: CoreMenu[] = [
-  {
-    id: 'home',
-    title: 'Home',
-    translate: 'MENU.HOME',
-    type: 'item',
-    icon: 'home',
-    url: 'home'
-  },
-  {
-    id: 'sample',
-    title: 'Sample',
-    translate: 'MENU.SAMPLE',
-    type: 'item',
-    icon: 'file',
-    url: 'sample'
-  },
-  // level
     {
-        id: 'levels',
-        title: 'Level',
-        translate: 'MENU.APPS.LEVEL.COLLAPSIBLE',
+        id: 'home',
+        title: 'Home',
+        translate: 'MENU.HOME',
+        type: 'item',
+        icon: 'home',
+        url: 'home'
+    },
+    {
+        id: 'sample',
+        title: 'Sample',
+        translate: 'MENU.SAMPLE',
+        type: 'item',
+        icon: 'file',
+        url: 'sample'
+    },
+    // competition
+    {
+        id: 'competitions',
+        title: 'Competition',
+        translate: 'MENU.APPS.COMPETITION.COLLAPSIBLE',
         type: 'collapsible',
         icon: 'bar-chart-2',
         children: [
-        {
-            id: 'list',
-            title: 'List',
-            translate: 'MENU.APPS.LEVEL.LIST',
-            type: 'item',
-            icon: 'circle',
-            url: 'level/list'
-        },
-        {
-            id: 'add',
-            title: 'Add',
-            translate: 'MENU.APPS.LEVEL.ADD',
-            type: 'item',
-            icon: 'circle',
-            url: 'level/add'
-        }
+            {
+                id: 'list',
+                title: 'List',
+                translate: 'MENU.APPS.COMPETITION.LIST',
+                type: 'item',
+                icon: 'circle',
+                url: 'competition/list'
+            },
+            {
+                id: 'add',
+                title: 'Add',
+                translate: 'MENU.APPS.COMPETITION.ADD',
+                type: 'item',
+                icon: 'circle',
+                url: 'competition/add'
+            },
         ]
     },
     // fish
@@ -67,6 +67,32 @@ export const menu: CoreMenu[] = [
             icon: 'circle',
             url: 'fish/add'
         }
+        ]
+    },
+    // level
+    {
+        id: 'levels',
+        title: 'Level',
+        translate: 'MENU.APPS.LEVEL.COLLAPSIBLE',
+        type: 'collapsible',
+        icon: 'bar-chart-2',
+        children: [
+            {
+                id: 'list',
+                title: 'List',
+                translate: 'MENU.APPS.LEVEL.LIST',
+                type: 'item',
+                icon: 'circle',
+                url: 'level/list'
+            },
+            {
+                id: 'add',
+                title: 'Add',
+                translate: 'MENU.APPS.LEVEL.ADD',
+                type: 'item',
+                icon: 'circle',
+                url: 'level/add'
+            }
         ]
     },
 ]
