585074a37ccc886e771a39d5bd60faf3913f99b2
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 09:08:42 +0100

    init CompetitionAddEditComponent

--- src/app/main/competition/pages/competition-add-edit/competition-add-edit.component.ts
+++ src/app/main/competition/pages/competition-add-edit/competition-add-edit.component.ts
@@ -1,13 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { NgbDateStruct, NgbCalendar, NgbDate, NgbDateParserFormatter, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from "@angular/router";
+import { FlatpickrOptions } from 'ng2-flatpickr';
+
+
+// services
+import { CompetitionService } from "../../service/competition.service";
 
 @Component({
   selector: 'app-competition-add-edit',
   templateUrl: './competition-add-edit.component.html',
-  styleUrls: ['./competition-add-edit.component.scss']
+  styleUrls: ['./competition-add-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CompetitionAddEditComponent implements OnInit {
+  public contentHeader: object;
 
-  constructor() { }
+  public competitionId: number;
+  public competitionCode = "";
+  public competitionAmount : number;
+  public competitionNumberOfParticipants : number;
+  public competitionDate: NgbDateStruct;
+  public fromDate: NgbDate | null;
+  public toDate: NgbDate | null;
+  public startTime;
+  public endTime;
+  public competitionLocation = "";
+  public   timeOptions: FlatpickrOptions = {
+    enableTime: true,
+    noCalendar: true,
+    altInput: true
+  }
+  public fileName = undefined;
+  public featuredImage: string;
+  isCompetitionCreated: boolean = false;
+  constructor(
+      private router: Router,
+      private competitionService: CompetitionService,
+      private calendar: NgbCalendar,
+      private formatter: NgbDateParserFormatter
+  ) { }
 
   ngOnInit(): void {
   }
