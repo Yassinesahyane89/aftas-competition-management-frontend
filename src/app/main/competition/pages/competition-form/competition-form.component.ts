7dcd0804924c2d18edff37c363be40b83e58a7a1
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 15:49:20 +0100

    refactoring code and solve problem of time

--- src/app/main/competition/pages/competition-form/competition-form.component.ts
+++ src/app/main/competition/pages/competition-form/competition-form.component.ts
@@ -41,7 +41,8 @@ export class CompetitionFormComponent implements OnInit {
   isDisabled = (date: NgbDate, current: { month: number; year: number }) => date.month !== current.month;
   isWeekend = (date: NgbDate) => this.calendar.getWeekday(date) >= 6;
 
-  // add uploadImage method
+  // add uploadImage 
+
   uploadImage(event: any) {
     if (event.target.files && event.target.files[0]) {
       let reader = new FileReader();
@@ -56,14 +57,30 @@ export class CompetitionFormComponent implements OnInit {
   }
 
   // add formatTime method for formatting time
-  formatTime(time: NgbTimeStruct) {
-    return new Date(this.competition.startTime).toLocaleTimeString('en-US', {
+  formatTime(time) {
+    if(time==null){
+      return null;
+    }
+    // here i want to do if time is 24
+    let timedate = new Date(time).toLocaleTimeString("en-US", {
       hour12: false,
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit'
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
     });
+
+    // here i want to check if timedate have 24 in houres replacet with 00
+    if (timedate.includes("24")) {
+      timedate = timedate.replace("24", "00");
+    }
+
+    return timedate;
   }
+
+  // add formatTime method for formatting time from  [Wed Dec 20 2023 00:00:00 GMT+0100 (GMT+01:00)] to HH:MM:SS
+    formatTimeFromNgbTimeStruct(time: NgbTimeStruct) {
+      return `${time.hour}:${time.minute}:${time.second}`;
+    }
 
   // add formatDate method for formatting date
   formatDate(date: NgbDateStruct) {
@@ -94,7 +111,7 @@ export class CompetitionFormComponent implements OnInit {
       customToastrRef.tapToDismiss = false;
       customToastrRef.progressBar = true;
       customToastrRef.toastClass = 'toast ngx-toastr';
-      this.toastr.error(error.error.message, 'Error!', customToastrRef);
+      this.toastr.error("Error!", 'Error!', customToastrRef);
     }else if (error && error.error){
       const validationErrors = error.error;
 
@@ -126,9 +143,7 @@ export class CompetitionFormComponent implements OnInit {
 
   // git commit -m "add methode updateCompetition for updating competition"
   updateCompetition(form) {
-    this.competition.startTime = this.formatTime(this.competition.startTime);
-    this.competition.endTime = this.formatTime(this.competition.endTime);
-    this.competition.date = this.formatDate(this.competition.date);
+    // create new
     this.competitionService.updateCompetition(this.competition)
         .subscribe((response :any) => {
           this.handleSuccess(response,form);
