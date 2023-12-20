fda567fb34cf5c638fa149fba58d371fc7850f9f
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 16:55:08 +0100

    show the card list of competition

--- src/app/main/competition/pages/competition-cardlist/competition-cardlist.component.ts
+++ src/app/main/competition/pages/competition-cardlist/competition-cardlist.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
+import { CompetitionService } from "../../service/competition.service";
 
 @Component({
-  selector: 'app-competition-cardlist',
-  templateUrl: './competition-cardlist.component.html',
-  styleUrls: ['./competition-cardlist.component.scss']
+  selector: "app-competition-cardlist",
+  templateUrl: "./competition-cardlist.component.html",
+  styleUrls: ["./competition-cardlist.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: "ecommerce-application" },
 })
 export class CompetitionCardlistComponent implements OnInit {
+  public contentHeader: object;
+  public competitions: any[];
+  public page = 1;
+  public pageSize = 6;
+  public searchText = "";
 
-  constructor() { }
+  constructor(private CompetitionService: CompetitionService) {}
 
-  ngOnInit(): void {
+  sortCompetitions(key) {
+    this.competitions.sort(key);
   }
 
+  // get all competitions
+  getCompetitions() {
+    this.CompetitionService.getAllCompetition()
+      .subscribe((competitions: any) => {
+        this.competitions = competitions.data;
+      });
+  }
+
+  ngOnInit(): void {
+    this.CompetitionService.getAllCompetition()
+        .subscribe((competitions: any) => {
+          this.competitions = competitions.data;
+        });
+
+    this.contentHeader = {
+      headerTitle: "Competitions",
+      actionButton: true,
+      breadcrumb: {
+        type: "",
+        links: [
+          {
+            name: "Home",
+            isLink: true,
+            link: "/",
+          },
+          {
+            name: "Competitions",
+            isLink: false,
+          },
+        ],
+      },
+    };
+  }
 }
