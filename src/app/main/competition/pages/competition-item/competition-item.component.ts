d8175b48c2a6fa462e4da0b5e775b50f8e192673
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 16:24:58 +0100

    add component of competition card

--- src/app/main/competition/pages/competition-item/competition-item.component.ts
+++ src/app/main/competition/pages/competition-item/competition-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-competition-item',
-  templateUrl: './competition-item.component.html',
-  styleUrls: ['./competition-item.component.scss']
+  selector: "app-competition-item",
+  templateUrl: "./competition-item.component.html",
+  styleUrls: ["./competition-item.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: "ecommerce-application" },
 })
 export class CompetitionItemComponent implements OnInit {
+  @Input() competition: any;
+  public dayOfWeek = "";
+  public dayOfMonth = "";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+     console.log("competition", this.competition);
+     const dateParts = this.competition.date.split(", ");
+     this.dayOfWeek = dateParts[0];
+     this.dayOfMonth = dateParts[1].split(" ")[1];
   }
-
 }
